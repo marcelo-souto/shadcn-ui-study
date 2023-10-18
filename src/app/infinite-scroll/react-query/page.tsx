3c3fcac52e5b3f0a100fca3b2fd44422efc52f22
marcelo souto <marcelosouto676@gmail.com>
2023-10-17 22:46:58 -0300

    -m

--- src/app/infinite-scroll/react-query/page.tsx
+++ src/app/infinite-scroll/react-query/page.tsx
@@ -2,6 +2,8 @@
 
 import { useIntersectionObserver } from "@/utils/hooks/use-intersection-observer";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import { forwardRef, useRef } from "react";
 
 type Product = {
   id: number;
@@ -26,9 +28,37 @@ type ProductResponse = {
 
 const BASE_URL = "https://dummyjson.com";
 
-export default function InfiniteQueryWithReactQuery() {
-  console.log("FUNCAO ACIONADA");
+const Product = forwardRef<HTMLLIElement, Product>(
+  ({ id, thumbnail, title, description, price }, ref) => {
+    const ref2 = useRef();
+    return (
+      <li
+        key={id}
+        className="rounded-lg border border-zinc-800 overflow-hidden shadow-xl shadow-zinc-950"
+        ref={ref}
+      >
+        <div className="relative aspect-video">
+          <Image
+            src={thumbnail}
+            alt={title}
+            fill={true}
+            className="object-cover object-center"
+          />
+        </div>
 
+        <div className="p-4">
+          <h2 className="font-semibold text-xl text-zinc-200 mb-4">{title}</h2>
+          <p className="line-clamp-2 mb-2 text-zinc-400">{description}</p>
+          <p className="font-semibold text-lg">R$ {price}</p>
+        </div>
+      </li>
+    );
+  }
+);
+
+Product.displayName = "Product";
+
+export default function InfiniteQueryWithReactQuery() {
   const fetchProducts = async (skip = 0, limit = 10) => {
     const response = await fetch(
       `${BASE_URL}/products?limit=${limit}&skip=${skip}`
@@ -42,16 +72,15 @@ export default function InfiniteQueryWithReactQuery() {
     queryKey: ["products"],
     queryFn: async ({ pageParam = 0 }) => fetchProducts(Number(pageParam)),
     getNextPageParam: (lastPage) => {
-      console.log(lastPage);
-
       const cursor = lastPage.skip + lastPage.limit;
-
-      if (lastPage.total >= cursor || lastPage.limit) return cursor;
+      if (lastPage.products.length) return cursor;
     },
     select: (data) => ({
       ...data,
       pages: data.pages.map((page) => page.products),
     }),
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
   });
 
   const lastProductRef = useIntersectionObserver<HTMLLIElement>(() => {
@@ -59,22 +88,18 @@ export default function InfiniteQueryWithReactQuery() {
   }, [hasNextPage]);
 
   return (
-    <div className="max-w-6xl mx-auto">
-      <ul className="grid grid-cols-3 gap-4">
+    <div className="max-w-6xl mx-auto pt-8 px-4">
+      <h1 className="text-xl sm:text-2xl md:text-4xl font-bold mb-8">
+        Produtos
+      </h1>
+
+      <ul className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-8">
         {data?.pages.flat().map((product, index, products) => (
-          <li
+          <Product
+            {...product}
             key={product.id}
-            className="p-4 rounded-lg border border-zinc-800"
-            ref={products.length - 1 === index ? lastProductRef : null}
-          >
-            <h2 className="font-semibold text-xl text-zinc-200 mb-4">
-              {product.title}
-            </h2>
-            <p className="line-clamp-3 mb-2 text-zinc-400">
-              {product.description}
-            </p>
-            <p className="font-semibold text-lg">R$ {product.price}</p>
-          </li>
+            ref={index === products.length - 1 ? lastProductRef : null}
+          />
         ))}
       </ul>
 
