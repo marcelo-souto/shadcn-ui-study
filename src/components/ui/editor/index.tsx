9d12c29d63bbea9cd43809375a037d9e634e6825
marcelo-souto <marcelosouto676@gmail.com>
2023-10-28 15:28:32 -0300

    uninstalling tip tap file handler

--- src/components/ui/editor/index.tsx
+++ src/components/ui/editor/index.tsx
@@ -5,7 +5,6 @@ import { useEditor, EditorContent } from "@tiptap/react";
 
 import TextAlign from "@tiptap/extension-text-align";
 import Dropcursor from "@tiptap/extension-dropcursor";
-import FileHandler from "@tiptap-pro/extension-file-handler";
 import Image from "@tiptap/extension-image";
 import StarterKit from "@tiptap/starter-kit";
 
@@ -38,59 +37,6 @@ const Editor = ({ content = "" }: EditorProps) => {
       }),
       Dropcursor.configure({
         color: "#a0aec0",
-      }),
-      FileHandler.configure({
-        allowedMimeTypes: [
-          "image/png",
-          "image/jpeg",
-          "image/gif",
-          "image/webp",
-        ],
-        onDrop: (currentEditor, files, pos) => {
-          files.forEach((file) => {
-            const fileReader = new FileReader();
-
-            fileReader.readAsDataURL(file);
-            fileReader.onload = () => {
-              currentEditor
-                .chain()
-                .insertContentAt(pos, {
-                  type: "image",
-                  attrs: {
-                    src: fileReader.result,
-                  },
-                })
-                .focus()
-                .run();
-            };
-          });
-        },
-        onPaste: (currentEditor, files, htmlContent) => {
-          files.forEach((file) => {
-            if (htmlContent) {
-              // if there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
-              // you could extract the pasted file from this url string and upload it to a server for example
-              console.log(htmlContent); // eslint-disable-line no-console
-              return false;
-            }
-
-            const fileReader = new FileReader();
-
-            fileReader.readAsDataURL(file);
-            fileReader.onload = () => {
-              currentEditor
-                .chain()
-                .insertContentAt(currentEditor.state.selection.anchor, {
-                  type: "image",
-                  attrs: {
-                    src: fileReader.result,
-                  },
-                })
-                .focus()
-                .run();
-            };
-          });
-        },
       }),
       Image.configure({
         allowBase64: true,
